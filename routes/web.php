57486068309428247a9264fa5433c0a1822cb1ee
tlegeek1524 <148260088+tlegeek1524@users.noreply.github.com>
2025-03-17 03:54:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SystemController;
 use App\Http\Controllers\DashboardController;
-Route::get('/', [SystemController::class,'index'])->name('home');
-Route::get('/sheet-data', [DashboardController::class, 'showSheetData'])->name('sheet.data');
-Route::get('/check-latest-data', [DashboardController::class, 'checkLatestData'])->name('check.latest.data');
-Route::get('/get-latest-data', [DashboardController::class, 'getLatestData'])->name('get.latest.data');
+Route::get('/', [SystemController::class, 'index'])->name('home');
+
+Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(function () {
+    Route::get('/sheet-data', [DashboardController::class, 'showSheetData'])->name('sheet.data');
+    Route::get('/check-latest-data', [DashboardController::class, 'checkLatestData'])->name('check.latest.data');
+    Route::get('/get-latest-data', [DashboardController::class, 'getLatestData'])->name('get.latest.data');
+});
